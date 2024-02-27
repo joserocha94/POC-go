c98e983b56f2ed725a4b3e74556a4cf9a9d6d46a
joserocha94 <joserocha94@outlook.com>
2024-02-27 22:43:45 +0000

    push

---
+++ src/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+
+
+
+func main() {
+
+    fmt.Println("Starting...\n")
+
+	test := &Graph{}
+	for i := 0; i < 3; i++ {
+		test.AddNode(i*10);
+	}
+
+	
+	test.Print()
+	test.AddEdge(0,10)
+	test.AddEdge(0,20)
+	test.AddEdge(10,20)
+	fmt.Println()
+
+	test.Print()
+	fmt.Println()
+
+	queue := &Queue{}
+	queue.id = 5
+
+	fmt.Printf("\n %d", queue.id)
+
+	fmt.Println("\nFinish")
+}
---
+++ src/node.go
@@ -0,0 +1,6 @@
+package structs
+
+type Node struct {
+	id int
+	adjacent []*Node
+}
